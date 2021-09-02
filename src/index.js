3650e1e784a36e7f45f4ddafb51d8249bfbbfcc7
coder-developer-a <coder.developer.a@gmail.com>
2021-09-03 02:33:29 +0430

    first commit

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename='/coder-developer-a.github.io/social'>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
