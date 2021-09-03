4267b89b5a41b61fbdec7d1d49c3f099dab9bf84
coder-developer-a <coder.developer.a@gmail.com>
2021-09-03 17:17:10 +0430

    fourth commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,9 +9,9 @@ const Header = () => {
     return (
         <div className='header d-flex justify-content-between align-items-center row set-position py-2'>
             <div className='col-12 col-sm-6 d-flex align-items-center justify-content-center justify-content-sm-start mb-2'>
-                <Link to='/' className='brand'><i className="bi bi-camera me-1"></i>Social</Link>
+                <Link to='/' className='brand text-nowrap'><i className="bi bi-camera pe-1"></i>Social</Link>
                 <div className="dropdown d-none d-sm-block">
-                    <button className="btn category-top pb-0 dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                    <button className="btn category-top pb-0 dropdown-toggle pe-0 m-0" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
                         Category
                     </button>
                     <ul className="dropdown-menu category-dropdown" aria-labelledby="dropdownMenuButton1">
@@ -22,7 +22,7 @@ const Header = () => {
                 </div>
                 <Link to="/photographers" className='btn category-top pb-0 d-none d-sm-block'>Photographers</Link>
             </div>
-            <div className='d-sm-none col-12'>
+            <div className='d-sm-none col-12 col-sm-6'>
                 <div className='row d-flex justify-content-center'>
                     <button className="navbar-toggler d-flex justify-content-center align-items-center category-top fs-5" type="button" data-bs-toggle="collapse" data-bs-target="#categoryCollapse" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <div className='d-inline fs-5'>Category</div>
@@ -49,12 +49,12 @@ const Header = () => {
                     <Link to='/signin' className='btn category-top fs-5'><span className='category-top fs-5'>Sign in</span></Link>
                 </div>
             </div>
-            <div className='col-12 col-sm-6'>
+            <div className='col-12 col-sm-5'>
                 <form className='d-flex align-items-center justify-content-center justify-content-sm-end'>
                     <div>
-                        <Link to='/signin'><button className='btn category-top d-none d-sm-inline-block'>Sign in</button></Link>
-                        <input type='text' className='search-inp' placeholder='Search ...' aria-label='Dollar amount (with dot and two decimal places)' />
-                        <span className='ms-2 search-span'><i className='bi bi-search category-top'></i></span>
+                        <Link to='/signin'><button className='btn category-top d-none d-sm-inline-block col-4 text-nowrap'>Sign in</button></Link>
+                        <input type='text' className='search-inp col-10 col-sm-7' placeholder='Search ...' aria-label='Dollar amount (with dot and two decimal places)' />
+                        <span className='search-span d-inline-block col-2 col-sm-1 text-end'><i className='bi bi-search category-top'></i></span>
                     </div>
                 </form>
             </div>
